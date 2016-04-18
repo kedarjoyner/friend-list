457f8ce40ca330154a66a511d79e266485955bd6
kedarjoyner <kedarjoyner@gmail.com>
2016-04-18 17:51:21 -0500

    initial setup

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+
+// include ejs files
+app.set("view engine", "ejs");
+
+// home page
+app.get("/", function(req, res){
+  res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
+  var campgrounds = [
+    {name: "Yosemite National Park", image: "https://unsplash.it/300?image=1044"},
+    {name: "Joshua Tree National Park", image: "https://unsplash.it/300?image=1011"},
+    {name: " Zion National Park", image: "https://unsplash.it/300?image=1016"}
+  ]
+  res.render("campgrounds", {campgrounds:campgrounds});
+});
+
+app.listen(3000, function() {
+  console.log("The YelpCamp Server has started!");
+});
---
+++ readme.md
@@ -0,0 +1,18 @@
+#YelpCamp
+
+* Add Landing page
+* Add Campgrounds Page that lists all campgrounds
+
+#Each campground has:
+  * Name
+  * Image
+
+[
+  {name:"Salmon Creek", image: "http://www.image.com"}
+  {name:"Salmon Creek", image: "http://www.image.com"}
+  {name:"Salmon Creek", image: "http://www.image.com"}
+  {name:"Salmon Creek", image: "http://www.image.com"}
+  {name:"Salmon Creek", image: "http://www.image.com"}
+  {name:"Salmon Creek", image: "http://www.image.com"}
+  {name:"Salmon Creek", image: "http://www.image.com"}
+]
