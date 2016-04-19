2de47ea35bef1211d8145d6ff5fbe5b1ec4e5de7
kedarjoyner <kedarjoyner@gmail.com>
2016-04-19 18:59:43 -0500

    add show page, description to form, and define routes

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ mongoose.connect("mongodb://localhost/yelp_camp");
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 // COMPILE SCHEMA INTO MODEL
@@ -18,8 +19,8 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 // Campground.create(
 //     {
 //       name: "Crater Lake National Park",
-//       image:"http://www.oregonwild.org/sites/default/files/crater-lake-justin-bailie-web.jpg"
-//
+//       image:"http://www.oregonwild.org/sites/default/files/crater-lake-justin-bailie-web.jpg",
+//       description: "This is a huge granite hill, no bathrooms. No water. Beautiful location."
 //     },
 //     function(err, campground) {
 //       if(err){
@@ -46,7 +47,7 @@ app.get("/", function(req, res){
 });
 
 
-// RETRIEVE FROM DB AND SHOW ALL CAMPGROUNDS ON CAMPGROUNDS.EJS
+// INDEX ROUTE - show all campgrounds
 app.get("/campgrounds", function(req, res){
 // Get all campgrounds from DB
   Campground.find({}, function(err, allCampgrounds){
@@ -55,25 +56,24 @@ app.get("/campgrounds", function(req, res){
     } else {
       // db will refer to them as campgrounds
       // allCampgrounds is defined
-      res.render("campgrounds", {campgrounds:allCampgrounds});
+      res.render("index", {campgrounds:allCampgrounds});
     }
   });
 });
 
+
+
 // diff then above url because this is post, not get
 // this follows REST convention
-// the route where you can create a new campground
 
-// MAKES NEW CAMPGROUND OBJECT
-// SAVES TO DB
-// IF NOT ERROR, PASSES CAMPGROUND GET WHERE IT'S DISPLAYED ON PAGE
-
+//CREATE ROUTE - add new campground to database
 app.post("/campgrounds", function(req, res){
   // get data from form and add to campgrounds array
   var name = req.body.name;
   var image = req.body.image;
+  var desc = req.body.description;
   // push new campgrounds into campground array
-  var newCampground = {name: name, image: image};
+  var newCampground = {name: name, image: image, description: desc};
   // Create new campground and save to DB
   // If this works, wIll then run code above in GET
   Campground.create(newCampground, function(err, newlyCreated){
@@ -87,9 +87,23 @@ app.post("/campgrounds", function(req, res){
   // campgrounds.push(newCampground);
 });
 
-//shows the form that sends the data to the above post route
+
+// NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res){
-  res.render("new.ejs");
+  res.render("new");
+});
+
+// SHOW - shows more info about campground
+app.get("/campgrounds/:id", function(req, res){
+  // find the campground with provided ID
+  Campground.findById(req.params.id, function(err, foundCampground){
+    // foundCampground is the data coming back from the DB
+      if(err){
+        console.log(err);
+      } else {
+        res.render("show", {campground:foundCampground});
+      }
+  });
 });
 
 app.listen(3000, function() {
--- readme.md
+++ readme.md
@@ -7,12 +7,15 @@
   * Name
   * Image
 
-[
-  {name:"Salmon Creek", image: "http://www.image.com"}
-  {name:"Salmon Creek", image: "http://www.image.com"}
-  {name:"Salmon Creek", image: "http://www.image.com"}
-  {name:"Salmon Creek", image: "http://www.image.com"}
-  {name:"Salmon Creek", image: "http://www.image.com"}
-  {name:"Salmon Creek", image: "http://www.image.com"}
-  {name:"Salmon Creek", image: "http://www.image.com"}
-]
+#Restful Routes
+
+name      url           verb        desc.
+===============================================================
+
+INDEX     /dogs         GET         Display a list of all dog
+
+NEW       /dogs/new     GET         Displays a form to make a new dog
+
+CREATE    /dogs         POST        Add new dog to DB
+
+SHOW      /dogs/:id     GET         Shows info about one dog
