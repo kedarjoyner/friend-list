15804fc595e7f89f45db818db627feb301dbcf29
kedarjoyner <kedarjoyner@gmail.com>
2016-04-18 22:37:31 -0500

    add styling to form

--- app.js
+++ app.js
@@ -12,9 +12,14 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 var campgrounds = [
-  {name: "Yosemite National Park", image: "https://unsplash.it/300?image=1044"},
-  {name: "Joshua Tree National Park", image: "https://unsplash.it/300?image=1011"},
-  {name: " Zion National Park", image: "https://unsplash.it/300?image=1016"}
+  {name: "Yosemite National Park", image: "http://travelercorner.com/wp-content/uploads/2016/02/Yosemite-Camping.jpg"},
+  {name: "Jumbo Rocks Campground", image: "http://cdn.c.photoshelter.com/img-get2/I0000JVhXQ5Wqock/fit=1000x750/Joshua-Tree-0640.jpg"},
+  {name: "Crater Lake National Park", image: "http://www.oregonwild.org/sites/default/files/crater-lake-justin-bailie-web.jpg"},
+  {name: "North Rim Campground", image: "http://cdn.roughguides.com/wp-content/uploads/2013/06/17.grandCanyon-86497002-1680x1050.jpg"},
+  {name: "Death Valley National Park", image: "https://c2.staticflickr.com/2/1311/5147976618_e3150a8532_b.jpg"},
+  {name: "White River Campground", image: "https://img.hipcamp.com/image/upload/c_limit,h_1200,q_60,w_1920/v1457727114/campground-photos/cwngj3nr0fzwrlgkatyj.jpg"},
+  {name: "Chisos Campground", image: "https://res-4.cloudinary.com/hipcamp/image/upload/c_limit,h_1200,w_1920/v1421954452/ucfrfcqks652jsch0cy6.jpg"},
+  {name: "Zion National Park", image: "http://dy7uvfdwot1mj.cloudfront.net/wp-content/uploads/2010/04/zion-park-beyondzion1.jpg"}
 ]
 
 // home page
