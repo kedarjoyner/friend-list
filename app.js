f323180cc4edc88ecec82c9d1e2d8f62ad3fa059
kedarjoyner <kedarjoyner@gmail.com>
2016-04-22 13:38:34 -0500

    associate campgrounds with unique ID

--- app.js
+++ app.js
@@ -1,106 +1,70 @@
-var express       = require("express"),
-    app           = express(),
-    bodyParser    = require("body-parser"),
-    mongoose      = require("mongoose"),
-    Campground    = require("./models/campground"),
-    seedDB        = require("./seeds");
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    Campground  = require("./models/campground"),
+    seedDB      = require("./seeds")
 
-//remove all campgrounds when page loads
+mongoose.connect("mongodb://localhost/yelp_camp");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 seedDB();
 
-// create yelpcamp database in mongodb
-mongoose.connect("mongodb://localhost/yelp_camp");
-
-// Campground.create(
-//     {
-//       name: "Crater Lake National Park",
-//       image:"http://www.oregonwild.org/sites/default/files/crater-lake-justin-bailie-web.jpg",
-//       description: "This is a huge granite hill, no bathrooms. No water. Beautiful location."
-//     },
-//     function(err, campground) {
-//       if(err){
-//         console.log(err);
-//       } else {
-//         console.log("NEWLY CREATED CAMPGROUND: ");
-//         console.log(campground);
-//       }
-// });
-
-// include body parser package
-app.use(bodyParser.urlencoded({extended:true}));
-
-// include public directory
-app.use(express.static("public"));
-
-// include ejs files
-app.set("view engine", "ejs");
-
-
-// home page
 app.get("/", function(req, res){
-  res.render("landing");
+    res.render("landing");
 });
 
-
-// INDEX ROUTE - show all campgrounds
+//INDEX - show all campgrounds
 app.get("/campgrounds", function(req, res){
-// Get all campgrounds from DB
-  Campground.find({}, function(err, allCampgrounds){
-    if(err){
-      console.log(err);
-    } else {
-      // db will refer to them as campgrounds
-      // allCampgrounds is defined
-      res.render("index", {campgrounds:allCampgrounds});
-    }
-  });
+    // Get all campgrounds from DB
+    Campground.find({}, function(err, allCampgrounds){
+       if(err){
+           console.log(err);
+       } else {
+          res.render("index",{campgrounds:allCampgrounds});
+       }
+    });
 });
 
-
-
-// diff then above url because this is post, not get
-// this follows REST convention
-
-//CREATE ROUTE - add new campground to database
+//CREATE - add new campground to DB
 app.post("/campgrounds", function(req, res){
-  // get data from form and add to campgrounds array
-  var name = req.body.name;
-  var image = req.body.image;
-  var desc = req.body.description;
-  // push new campgrounds into campground array
-  var newCampground = {name: name, image: image, description: desc};
-  // Create new campground and save to DB
-  // If this works, wIll then run code above in GET
-  Campground.create(newCampground, function(err, newlyCreated){
-    if(err){
-      console.log(err);
-    } else {
-      // redirect to campgrounds page
-      res.redirect("/campgrounds");
-    }
-  });
-  // campgrounds.push(newCampground);
+    // get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc}
+    // Create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            //redirect back to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
-
-// NEW - show form to create new campground
+//NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res){
-  res.render("new");
+   res.render("new.ejs");
 });
 
-// SHOW - shows more info about campground
+// SHOW - shows more info about one campground
 app.get("/campgrounds/:id", function(req, res){
-  // find the campground with provided ID
-  Campground.findById(req.params.id, function(err, foundCampground){
-    // foundCampground is the data coming back from the DB
-      if(err){
-        console.log(err);
-      } else {
-        res.render("show", {campground:foundCampground});
-      }
-  });
-});
+    //find the campground with provided ID
+    // populate the comments on that campground
+    // .exec executes the query we just made
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            console.log(foundCampground)
+            //render show template with that campground
+            res.render("show", {campground: foundCampground});
+        }
+    });
+})
 
-app.listen(3000, function() {
-  console.log("The YelpCamp Server has started!");
+app.listen(3000, function(){
+   console.log("The YelpCamp Server Has Started!");
 });
